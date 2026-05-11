b72e93a5d7ebc25d4e680e6ea17b74beaf129273
Nexux21 <jesusserranocosio300@gmail.com>
2026-05-11 11:18:09 -0500

    Update ejercicio 6.cs

--- Assets/scripts/ejercicio 6.cs
+++ Assets/scripts/ejercicio 6.cs
@@ -5,6 +5,9 @@ public class ejercicio6 : MonoBehaviour
     string[] puntajes = { "16", "17","18","19","20" };
     void Start()
     {
+        int i = 0;
+
+        while (i < puntajes.Length);
         
     }
 
