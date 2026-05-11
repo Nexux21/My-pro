81a89d3311afddb884884ebfef74e0d3b9cee264
Nexux21 <jesusserranocosio300@gmail.com>
2026-05-11 11:13:55 -0500

    Update ejercicio 6.cs

--- Assets/scripts/ejercicio 6.cs
+++ Assets/scripts/ejercicio 6.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ejercicio6 : MonoBehaviour
 {
-    //string[] puntajes = { "16","17",}
+    string[] puntajes = { "16", "17","18","19","20" };
     void Start()
     {
         
