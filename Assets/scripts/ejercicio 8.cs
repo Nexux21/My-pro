6e6e50af09e33c6a5771d39ce4046c5bc020fd7d
Nexux21 <jesusserranocosio300@gmail.com>
2026-05-17 19:59:49 -0500

    Update ejercicio 8.cs

--- Assets/scripts/ejercicio 8.cs
+++ Assets/scripts/ejercicio 8.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
 
-public class ejercicio8 : MonoBehaviour
+public class Ejercicio8 : MonoBehaviour
 {
+    
+    int[] vidasEnemigos = { 100, 0, 45, 0, 80, 12 };
 
     void Start()
     {
-        
-    }
+        Debug.Log("--- LISTA DE ENEMIGOS VIVOS ---");
 
-    void Update()
-    {
-       
+        
+        for (int i = 0; i < vidasEnemigos.Length; i++)
+        {
+            
+            if (vidasEnemigos[i] > 0)
+            {
+                
+                Debug.Log("Enemigo [" + i + "] está VIVO con " + vidasEnemigos[i] + " de vida.");
+            }
+        }
     }
 }
